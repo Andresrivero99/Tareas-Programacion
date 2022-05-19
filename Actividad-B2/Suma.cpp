9021f3e5afa26b820489736a947216d4379309bf
Andres Rivero <93391282+Andresrivero99@users.noreply.github.com>
2022-05-19 09:13:09 -0500

    Add files via upload

---
+++ Actividad-B2/Suma.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+
+{
+	
+	int a,y,x;
+	
+	cout<<"ingrese la primera variable";
+	cin>>a;
+	cout<<"ingrese la segunda variable";
+	cin>>y;
+	
+	x=a+y;
+	
+	cout<<"La suma es:"<<x<<endl;
+	
+	
+	
+return 0; // sirve para realizar sumas 
+}

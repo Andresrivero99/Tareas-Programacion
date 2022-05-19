4bb8f9dc89a8cb0f1a5fd1ab35f8ef86d41057b8
Andres Rivero <93391282+Andresrivero99@users.noreply.github.com>
2022-05-19 09:32:47 -0500

    Add files via upload

---
+++ Actividad B2/Rivero Andres/comparar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,y ;
+	cout<<"Ingrese los numeros a comparar"<<endl;
+	cin>> a;
+	cin>> y;
+	if (a==y)
+	{
+		cout<<"iguales"<<endl;
+	}
+else if (a>y)
+		{
+			cout<<a<<" es mayor"<<endl;
+		}
+		else{
+			cout<<y<<"es mayor"<<endl;
+}
+	
+	return 0;//sirve para saber que numero es mayor
+}

bf29134177061209228e11dfb668120cfbe6a56a
Andres Rivero <93391282+Andresrivero99@users.noreply.github.com>
2022-05-22 23:10:56 -0500

    Add files via upload

---
+++ Actividad C2/Rivero Andres/RiveroAndres-compara.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int ra_a,ra_y ;
+	cout<<"Ingrese los numeros a comparar"<<endl;
+	cin>>ra_a;
+	cin>>ra_y;
+	if (ra_a==ra_y)
+	{
+		cout<<"iguales"<<endl;
+	}
+else if (ra_a>ra_y)
+		{
+			cout<<ra_a<<" es mayor"<<endl;
+		}
+		else{
+			cout<<ra_y<<"es mayor"<<endl;
+			
+	cout<<"//==>nombre del programa compara: andres rivero"<<endl;
+	cout<<"//==>archivo:compara.cpp"<<endl;
+	cout<<"//==>fecha de elaboracion:  2022-05-02"<<endl;
+	cout<<"//==>fecha de la ultima actualizacion: 2022.05-20"<<endl;
+}
+
+	return 0;//sirve para comparar que numero es mayor
+}
